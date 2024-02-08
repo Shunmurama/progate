29f8d13109d73976a8959a27fb2a7aa41ac103bc
Shunmurama <mura.faily72@gmail.com>
2024-02-08 19:43:11 +0000

    lesson1

--- Java/First.java
+++ Java/First.java
@@ -3,6 +3,20 @@ class First {
     System.out.println("こんにちは");
     System.out.println(3 +6);
     System.out.println("こんにちは" + "世界");
+    // 変数定義
+    // 文字列
+    String name;
+    name = "Gon";
+    System.out.println(name);
+    // 整数型
+    int number = 3;
+    System.out.println(number);
+    // 変数を上書きするときはデータ型はいらない
+    number = 5;
+    System.out.println(number);
+    // 自己代入
+    number = number + 4;
+    System.out.println(number);
   }
 }
 
