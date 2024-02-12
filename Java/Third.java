334f5c968060e230d6ba411e993878f7e9bdb489
Shunmurama <mura.faily72@gmail.com>
2024-02-12 18:24:46 +0000

    lesson3

--- Java/Third.java
+++ Java/Third.java
@@ -1,15 +1,23 @@
 class Third {
   public static void main(String[] args){
-    hello();
-    java();
+    hello("Gon", 21);
+    total(3, 7);
   }
 
-    public static void hello(){
-      System.out.println("こんにちは");
+
+
+    // メソッド名(データ型 引数)
+    public static void hello(String name, int age){
+      System.out.println("こんにちは"+ name + "さん");
+      System.out.println(age + "歳");
     }
 
-    public static void java(){
-      System.out.println("Java");
+    public static void total(int number1, int number2){
+      int num = add(number1, number2);
+      System.out.println(num);
     }
 
+    public static int add(int number1, int number2){
+      return number1 + number2;
+    }
 }
