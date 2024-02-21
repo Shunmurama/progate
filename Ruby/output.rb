4a7d4c5aa9334754009221e515cc0baaa8905298
Shunmurama <mura.faily72@gmail.com>
2024-02-21 09:44:53 +0000

    メソッド勉強

--- Ruby/output.rb
+++ Ruby/output.rb
@@ -30,9 +30,31 @@ p x.shift
 # unshiftで先頭に追加
 p x.unshift("python")
 # spliy(指定文字)で指定した文字で区切ることができる
-w = "Ruby,Python, Java"
+w = "Ruby, Python, Java"
 p w
 p w.split(",")
 # 文字指定しなければ１文字ずつで区切る
 p w.split("")
+p w.split(",")
+p w
+# sliceで指定の要素を取り出す
+p w.slice(0)
 
+number = [1,5,26,3,88,542,235,2]
+# sort順番を並び替える
+p number.sort
+
+# 再帰関数基礎　フィボナッチ関数
+def fibo(num)
+  if num == 0
+    return 0
+  elsif num == 1
+    return 1
+  else
+    fibo(num - 1) + fibo(num - 2)
+  end
+end
+
+(1..10).each do |i|
+  puts "#{i}回目の計算: #{fibo(i)}"
+end
