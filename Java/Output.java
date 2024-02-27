19bed60bac85096f7b958cde50dd8a8423dc2ba7
Shunmurama <mura.faily72@gmail.com>
2024-02-27 12:49:58 +0000

    scanner

--- Java/Output.java
+++ Java/Output.java
@@ -1,18 +1,26 @@
+import java.util.Scanner;
+
 class Output {
+  // voidはデータ型のかわりみたいなもん
   public static void main(String[] args){
+    Scanner scanner = new Scanner(System.in);
     System.out.println("こんにちは");
 
     // 変数定義　データ型　変数名
-    int number1 = 10;
-    System.out.println(number1);
+    System.out.println("数字を入力します");
+    int number1 = scanner.nextInt();
+    System.out.println(number1 + "です");
 
     // 更新の時はデータ型なし
-    number1 = 2;
-    System.out.println(number1);
+    System.out.println("再度数字の変更をします");
+    number1 = scanner.nextInt();
+    System.out.println(number1 + "に変更しました");
 
-    int number2 = 4;
+    System.out.println("数字を追加します");
+    int number2 = scanner.nextInt();
     // int型でも(double)を前につけることでdoubleとして扱える
     System.out.println((double)number1 * number2);
 
   }
+
 }
